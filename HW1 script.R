716dbf6e0ef18ce28c8dde7a2b99f3dcb464734d
Rachael <rachaellatimer@gmail.com>
2021-10-19 22:40:59 -0700

    Task 1.3, start of 1.4

--- HW1 script.R
+++ HW1 script.R
@@ -25,3 +25,35 @@ tweet_data$date <- as.numeric(substr(tweet_data$time,9,10))
 #pull out hour, convert to numeric
 tweet_data$hour <- as.numeric(substr(tweet_data$time,12,13))
 
+#printing frequencies of new variables
+#probably a more elegant way to do this but this works for now
+table(day)
+table(month)
+table(date)
+table(hour)
+
+
+#Task1.3
+#Recode the outcome variable (sentiment) into a binary variable such that 
+#Positive is equal to 1 and Negative is equal to 0. 
+#Calculate and print the frequencies for tweets with positive and negative sentiments.
+
+tweet_data$sentiment <- dplyr::recode(tweet_data$sentiment,
+                                      'Positive'=1,
+                                      'Negative'=0)
+table(tweet_data$sentiment)
+
+#Task1.4
+#loading packages and Python libraries
+require(reticulate)
+
+reticulate::import('torch')
+reticulate::import('numpy')
+reticulate::import('transformers')
+reticulate::import('nltk')
+reticulate::import('tokenizers')
+
+require(text)
+
+#generate tweet embeddings for each tweet in this dataset using the roberta-base model
+
